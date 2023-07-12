2ed3f957d2795f8f31c7e9a7f25a0adb0b28fb0e
陈勃然 <Ran13245@outlook.com>
2023-07-13 00:24:11 +0800

    按键停止

--- User/key_handle.c
+++ User/key_handle.c
@@ -1,11 +1,18 @@
 #include "bsp.h"
 
+uint8_t tmp[5]={0x2c,0x12,0x01,0x01,0x5b};
+
 void KEY0_CallBack(void){
     printf_user(CONSOLE_UART,"key 0 pressed\r\n");
+    tmp[3]=0x01;
+    Uart_DMA_Trans(K210_UART,tmp,5);
 }
 
 void KEY1_CallBack(void){
     printf_user(CONSOLE_UART,"key 1 pressed\r\n");
+    tmp[3]=0x00;
+    Uart_DMA_Trans(K210_UART,tmp,5);
+    Set_Car_Attitude(0,0);
 }
 
 void KEY2_CallBack(void){
